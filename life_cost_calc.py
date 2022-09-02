881c6d45d3005cbd2b5239c79ea0c0bd9cc5de6b
YuyaIsh <101812706+YuyaIsh@users.noreply.github.com>
2022-09-02 11:42:52 +0900

    Add files via upload

--- life_cost_calc.py
+++ life_cost_calc.py
@@ -55,6 +55,7 @@ def main():
         # ログ確認&削除
     with tab_edit:
         df_concat = pd.concat([df_current_month, df_pre_month])
+        df_concat = df_concat.sort_values("日付",ascending=False)
         col_delete,col_df = st.columns(2)
 
         with col_delete:
